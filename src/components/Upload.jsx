1dfd4c8e00781808a066cca10247ee2c19e6c94d
Shyam <butanishyam111@gmail.com>
2022-12-27 09:43:29 +0530

    Video upload page completed

---
+++ src/components/Upload.jsx
@@ -0,0 +1,37 @@
+import { Button, Container, HStack, Input, VStack } from '@chakra-ui/react';
+import React from 'react';
+import { AiOutlineCloudUpload } from 'react-icons/ai';
+
+const Upload = () => {
+  return (
+    <Container w={'container.xl'} h="100vh" p={'16'}>
+      <VStack color={'purple.500'} h="full" justifyContent={'center'}>
+        <AiOutlineCloudUpload size={'10vmax'} />
+
+        <form>
+          <HStack>
+            <Input
+              type={'file'}
+              required
+              css={{
+                '&::file-selector-button': {
+                  width: 'calc(100% + 36px)',
+                  height: '100%',
+                  color: 'purple',
+                  border: 'none',
+                  cursor: 'pointer',
+                  backgroundColor: 'white',
+                },
+              }}
+            />
+            <Button type={'submit'} colorScheme={'purple'}>
+              Upload
+            </Button>
+          </HStack>
+        </form>
+      </VStack>
+    </Container>
+  );
+};
+
+export default Upload;
